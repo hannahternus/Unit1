8f418348be08215f1c745970d9c88cf92801336f
hannahternus <829839@mystma.org>
2020-09-22 04:08:20 +0000

    Data types

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,10 +2,8 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. Full Metal Jacket");
-        System.out.println("2. Divergent Trilogy");
-        System.out.println("3. Karate Kid 2010");
-        System.out.println("4. Baywatch");
-        System.out.println("5. Maze Runner series");
+        
+
+    
     }
 }
